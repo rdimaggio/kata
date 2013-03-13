59854337281d26b893e1b28c909af35b5dc402c1
rdimaggio <robert.dimaggio@gmail.com>
2013-03-12 23:21:52 -0700

    added string functionality

--- kata11/kata11.py
+++ kata11/kata11.py
@@ -1,10 +1,11 @@
 import unittest
 from random import randrange
+import string
 
 
 class Rack():
     def __init__(self):
-        self.balls = []
+        self.items = []
 
     def quick_sort(self, sort_list):
         if sort_list == []:
@@ -15,9 +16,18 @@ class Rack():
             greater = self.quick_sort([x for x in sort_list if x >= pivot])
             return lesser + [pivot] + greater
 
-    def add(self, ball):
-        self.balls.append(ball)
-        self.balls = self.quick_sort(self.balls)
+    def add(self, item):
+        if isinstance(item, basestring):
+            item = item.replace(' ', '').lower().translate(None,
+                                                           string.punctuation)
+            for each in item:
+                self.items.append(each)
+        else:
+            self.items.append(item)
+        self.items = self.quick_sort(self.items)
+
+    def items_string(self):
+        return ''.join(self.items)
 
 
 class TestSequenceFunctions(unittest.TestCase):
@@ -26,20 +36,25 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test(self):
         rack = Rack()
-        self.assertEqual([], rack.balls)
+        self.assertEqual([], rack.items)
         rack.add(20)
-        self.assertEqual([20], rack.balls)
+        self.assertEqual([20], rack.items)
         rack.add(10)
-        self.assertEqual([10, 20], rack.balls)
+        self.assertEqual([10, 20], rack.items)
         rack.add(30)
-        self.assertEqual([10, 20, 30], rack.balls)
+        self.assertEqual([10, 20, 30], rack.items)
         rack.add(20)
-        self.assertEqual([10, 20, 20, 30], rack.balls)
+        self.assertEqual([10, 20, 20, 30], rack.items)
         rack.add(5)
-        self.assertEqual([5, 10, 20, 20, 30], rack.balls)
+        self.assertEqual([5, 10, 20, 20, 30], rack.items)
         rack.add(2)
-        self.assertEqual([2, 5, 10, 20, 20, 30], rack.balls)
+        self.assertEqual([2, 5, 10, 20, 20, 30], rack.items)
 
+        rack = Rack()
+        rack.add('When not studying nuclear physics, Bambi likes to play ' +
+                 'beach volleyball')
+        self.assertEqual('aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsss' +
+                         'stttuuvwyyyy', rack.items_string())
 
 if __name__ == '__main__':
     unittest.main()
